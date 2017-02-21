49ffd54da4d90ec9071332245c39e13d5af1d6d5
Susha Dore <susha.dore@gmail.com>
2017-02-20 19:14:26 -0800

    add an === else if statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 $(function() {
   var age = parseInt(prompt("How old are you?"));
 
-  if (age >= 18) {
+  if (age > 18) {
     $("#old-enough").show();
-  } else {
+  } else if (age===18) {
+    $("#just-18").show();
+  }
+  else {
     $("#too-young").show();
   }
 });
